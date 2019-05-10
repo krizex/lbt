6bb40289f11277e727caa8b68326ec797bd0eb7c
Yang Qian <yang.qian@citrix.com>
2019-05-11 04:03:00 +0800

    only run once a day
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- src/quant/server/run.py
+++ src/quant/server/run.py
@@ -1,5 +1,7 @@
 import threading
+import gc
 import time
+import datetime
 from quant import dummy_web_server
 from quant.trend import find_chances
 from quant.server.chances import write_chances
@@ -11,22 +13,35 @@ class WebServerThread(threading.Thread):
         dummy_web_server.run(port=8000)
 
 
+def is_time_to_run(prev, now, expt):
+    return prev <= expt <= now
+
+
 def main():
     web_t = WebServerThread()
     web_t.start()
 
     d_from = '2016-01-01'
+
+    prev_check = datetime.datetime.now()
     while True:
-        try:
-            start = time.time()
-            stocks = find_chances(d_from, None, 22)
-            end = time.time()
-            write_chances(stocks, end - start)
-        except KeyboardInterrupt:
-            raise
-        except:
-            log.exception('Fail to write chances')
-            time.sleep(10)
+        gc.collect()
+        expect = datetime.datetime(prev_check.year, prev_check.month, prev_check.day, hour=15, minute=30, second=0)
+        now_check = datetime.datetime.now()
+        if is_time_to_run(prev_check, now_check, expect):
+            try:
+                start = time.time()
+                stocks = find_chances(d_from, None, 22)
+                end = time.time()
+                write_chances(stocks, end - start)
+            except KeyboardInterrupt:
+                raise
+            except:
+                log.exception('Fail to write chances')
+        else:
+            time.sleep(60)
+
+        prev_check = now_check
 
 
 if __name__ == '__main__':
